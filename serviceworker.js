378bd5b02284c1a5493d817bf4eea4c1f8ab1e05
elias.paiva <elias.paiva@ideagri.com.br>
2020-09-27 02:31:32 -0300

    Inicio do Service Worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,2 +1,7 @@
-self.addEventListener('install', () => { console.log('Nárnia'); });
+self.addEventListener('install', () => {
+    if (window.caches) {
+        caches.open
+    }
+});
 self.addEventListener('activate', () => { console.log('Arquelândia'); });
+self.addEventListener('fetch', () => { console.log('Arquelândia'); });
