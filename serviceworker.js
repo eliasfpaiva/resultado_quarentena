cd0cc654efbc246dcb3e9a21a2c56ff57f485d54
unknown <eliaspaiva@irmaos.com>
2020-10-08 00:19:33 -0300

    Ajuste no qrcode e na imagem da balançca

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 //  Define o nome e versão do cache atual
-var nomeCache = 'cache-RQ-3.2';
+var nomeCache = 'cache-RQ-3.5';
 
 // Variável de controle de atualizações do Service Worker
 // Inicializo com 32, para garantir a atualização ao primeiro uso.
